8c1ce42d1c8ad7b8848685aab8c5f17987eda6bd
NikitaMatv22 <114880758+NikitaMatv22@users.noreply.github.com>
2022-10-18 14:33:57 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4.8
+            //По По номеру дня недели вывести его название.
+
+            Console.WriteLine("введите номер дня недели ");
+            int a =Int32.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("понедельник");
+                    break;
+                case 2:
+                    Console.WriteLine("вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("среда");
+                    break;
+                case 4:
+                    Console.WriteLine("четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("воскресенье");
+                    break;
+               
+
+                default:
+                    Console.WriteLine("Нет такого дня недели");
+                    break;
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
